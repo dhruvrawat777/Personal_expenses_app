b4ba98ec80911ed14495f5afc6890ecb6ede3991
Dhruv <dhruvrawat2611@gmail.com>
2020-09-16 01:29:55 +0530

    displayed transactions using ListView.separated

--- lib/ui/Transactions.dart
+++ lib/ui/Transactions.dart
@@ -10,11 +10,49 @@ class Transactions extends StatelessWidget {
     final td = Provider.of<TransactionList>(context);
     final transactionlist = td.transactions;
     print(transactionlist);
-    return ListView.builder(
-      itemBuilder: (ctx, index) {
-        return Text(transactionlist[index].amount.toString());
-      },
-      itemCount: transactionlist.length,
+    return Column(
+      children: [
+        Card(
+          child: Text('hgi'),
+        ),
+        Expanded(
+          //height: MediaQuery.of(context).size.height - 50,
+          child: ListView.separated(
+            separatorBuilder: (ctx, index) {
+              return Divider(
+                color: Colors.black54,
+              );
+            },
+            itemBuilder: (ctx, index) {
+              //return Text(transactionlist[index].amount.toString());
+              return Container(
+                width: 200,
+                height: 100,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.account_box,
+                    size: 80,
+                  ),
+                  title: Container(
+                    padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child: Text(
+                      transactionlist[index].title,
+                      style: TextStyle(
+                        fontSize: 35,
+                      ),
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Rs: ' + transactionlist[index].amount.toString(),
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              );
+            },
+            itemCount: transactionlist.length,
+          ),
+        ),
+      ],
     );
   }
 }
